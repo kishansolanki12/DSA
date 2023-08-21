f278eb21be2b68055c08431453ad82ffb38e9e65
kishansolanki12 <139319242+kishansolanki12@users.noreply.github.com>
2023-08-21 11:56:17 +0530

    21-08-2023

---
+++ firstinsert.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#define n 5
+int a[n];
+int top = -1, i;
+int display()
+{
+  if (top < 0)
+  {
+    printf("\nArray is empty .....");
+  }
+  else
+  {
+    for (i = 0; i <= top; i++)
+    {
+      printf("  %d", a[i]);
+    }
+  }
+}
+
+int firstInsert(int value)
+{
+  if (top >= n - 1)
+  {
+    printf("\n Array is full :");
+    
+  }
+  else
+  {
+    for (int i = top; i >= 0; i--)
+    {
+      a[i + 1] = a[i];
+    }
+    a[0] = value;
+    top++;
+    
+  }
+}
+
+int firstDelete()
+{
+  int x;
+  if (top < 0)
+  {
+    printf("\n Array is already empty :");
+   
+  }
+  else
+  {
+    x = a[0];
+    printf("\n");
+    for (int i = 0; i < top; i++)
+    {
+      a[i] = a[i + 1];
+    }
+    top--;
+    return x;
+  }
+}
+
+int insertEnd(int val)
+{
+  if (top >= n - 1)
+  {
+    printf("\n Array is full :");
+  }
+  else
+  {
+    top++;
+    a[top] = val;
+  }
+}
+
+int delete()
+{
+  int x;
+  if (top < 0)
+  {
+    printf("Array is already empty :");
+  }
+  else
+  {
+    x = a[top];
+    top--;
+    return x;
+  }
+}
+
+
+int main()
+{
+  firstInsert(100);
+  firstInsert(200);
+  firstInsert(300);
+  firstInsert(400);
+  firstInsert(500);
+  firstInsert(600);
+  display();
+  firstDelete();
+  display();
+  
+}  
+
+
+
